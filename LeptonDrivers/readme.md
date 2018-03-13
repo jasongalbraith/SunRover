ea6934cea8fadce26b03067f2ba096d3a172d8a7
carsoncummins11 <carson.cummins11@gmail.com>
2018-03-13 19:18:06 +0000

    added IP adress of laptop, binds correctly to laptop

--- LeptonDrivers/readme.md
+++ LeptonDrivers/readme.md
@@ -1,4 +1,4 @@
 In the drivers to read data from the lepton data, enter in to the file
-software/raspberrypi_video. The replace the file MyLabel.h and
-MyLabel.cpp with these provided files. This will allow the data
+software/raspberrypi_video. The replace the files MyLabel.h and
+MyLabel.cpp with these provided files. Additionally, add sSocket.o, sSocket.h, and sSocket.o to the folder. Rebuild. This will allow the data
 to be sent over the network.
---
+++ LeptonDrivers/sSocket.cpp
@@ -0,0 +1,26 @@
+#include "sSocket.h"
+
+sSocket::sSocket() {
+	struct sockaddr_in address;
+	sock = 0;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return;
+	}
+	memset(& serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	if(inet_pton(AF_INET,"172.30.87.106",&serv_addr.sin)<=0){
+	printf("\nConnection Failed \n");
+	return -1;
+	}
+	if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\nConnection Failed \n");
+		return;
+	}
+	return;
+}
+sSocket::~sSocket(){}
+void sSocket::Send(char* str) {
+ 	send(sock, str, strlen(str), 0);
+}
